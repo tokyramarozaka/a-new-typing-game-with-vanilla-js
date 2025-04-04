15645e3e82c74c262304c019d6661c855b5d0aec
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-04-04 20:25:31 +0200

    FIx: dropdown menu js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,1 +1,4 @@
-const profileDropdown = document.querySelector("profile-dropdown-list");
+const profileDropdownList = document.querySelector(".profile-dropdown-list");
+const btn = document.querySelector(".profile-dropdown-btn");
+
+const toggle = ()=>profileDropdownList.classList.toggle('active');
