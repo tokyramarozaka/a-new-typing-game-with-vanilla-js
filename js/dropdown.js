03ee1645d72a5b4c869278c9efb476d5e5a0c88c
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-04-04 21:10:40 +0200

    Header avec js

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,3 +2,8 @@ const profileDropdownList = document.querySelector(".profile-dropdown-list");
 const btn = document.querySelector(".profile-dropdown-btn");
 
 const toggle = ()=>profileDropdownList.classList.toggle('active');
+
+window.addEventListener('click',(e)=>{
+    if(!btn.contains(e.target)) profileDropdownList.classList.remove('active')
+
+})
