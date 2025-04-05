f0d7c234f4b4b5d2021da8cc72cc3fd1b42d793d
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-04-05 13:55:51 +0200

    feat: navbar finalisée avec Bootstrap

--- js/dropdown.js
+++
@@ -1,9 +0,0 @@
-const profileDropdownList = document.querySelector(".profile-dropdown-list");
-const btn = document.querySelector(".profile-dropdown-btn");
-
-const toggle = ()=>profileDropdownList.classList.toggle('active');
-
-window.addEventListener('click',(e)=>{
-    if(!btn.contains(e.target)) profileDropdownList.classList.remove('active')
-
-})
